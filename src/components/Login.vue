edf056bc094d5614b8125e0fc059e0169a9ca0ca
Adrian Ramiro Gay Cattaneo <cattanea@viacommix.com>
2018-05-08 13:20:42 -0300

    Add login screen

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Labs Sportsbook Login</h1>
+
+    <form>
+      <label for="username">Username</label>
+      <input type="text" id="username">
+      <label for="password">Password</label>
+      <input type="password" id="password">
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      msg: 'Welcome to Labs Sportsbook App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
