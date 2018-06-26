09e97b85f1377f64f91cb7d7bbb72e84450b9b86
Ale R Buteler <alejandro.buteler@endava.com>
2018-06-26 17:29:50 -0300

    Encapsulates the navigational tabs as a component and adds a new Home icon to it.

---
+++ src/components/AppNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <div>
+      <md-tabs>
+        <md-tab id="tab-home"
+                md-label="Home" md-icon="home"
+                to="/"></md-tab>
+        <md-tab id="tab-upcoming-challenges"
+                md-label="Upcoming Challenges" md-icon="today"
+                to="/components/upcoming"></md-tab>
+        <md-tab id="tab-my-challenges"
+                md-label="My Challenges" md-icon="perm_contact_calendar"
+                to="/components/my-challenges"></md-tab>
+        <md-tab id="tab-rankings"
+                md-label="Rankings" md-icon="list_alt"
+                to="/components/rankings"></md-tab>
+        <md-tab id="tab-profile"
+                md-label="Profile" md-icon="account_circle"
+                to="/components/profile"></md-tab>
+      </md-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNav',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+  /*min-width: 900px;*/
+}
+h1 {
+  font-weight: normal;
+}
+</style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,24 +2,16 @@
   <div class="wrapper">
     <h1>Teambapp Dashboard</h1>
     <br />
-    <div>
-      <md-tabs>
-        <md-tab id="tab-upcoming-challenges"
-                md-label="Upcoming Challenges" md-icon="today"></md-tab>
-        <md-tab id="tab-my-challenges"
-                md-label="My Challenges" md-icon="perm_contact_calendar"></md-tab>
-        <md-tab id="tab-rankings"
-                md-label="Rankings" md-icon="list_alt"></md-tab>
-        <md-tab id="tab-profile"
-                md-label="Profile" md-icon="account_circle"></md-tab>
-      </md-tabs>
-    </div>
+    <AppNav></AppNav>
   </div>
 </template>
 
 <script>
+import AppNav from './AppNav';
+
 export default {
   name: 'Dashboard',
+  components: { AppNav },
   data() {
     return {
       msg: 'Welcome to Labs Dashboard Teambapp App',
